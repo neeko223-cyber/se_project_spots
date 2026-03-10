8079a1ee3d294418ac984409e3aa2a7d1512cd18
Nicholas Lawrence <neeko223@gmail.com>
2026-03-09 22:58:10 -0400

    "new update for my project"

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -2,7 +2,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const { use } = require("react");
+
 
 module.exports = {
   entry: {
